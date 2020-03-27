25d9f1070fd2151a53f193cddacbac6a84c82725
Rick <givemepassxd999@gmail.com>
2020-03-27 16:36:01 +0800

    the third version

--- app/src/main/java/com/sample/demo/myapplication/InfoViewModel.kt
+++ app/src/main/java/com/sample/demo/myapplication/InfoViewModel.kt
@@ -2,27 +2,39 @@ package com.sample.demo.myapplication
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class InfoViewModel() : ViewModel() {
-    suspend fun step1(isSuccess: Boolean) = withContext(Dispatchers.IO) {
-        if (isSuccess) {
-            Log.d("rick", "step1")
-        } else {
-            Log.d("rick", "step1 nothing")
+    interface JobStatus {
+        fun jobDone(result: Boolean)
+    }
+
+    fun exeTask(): JobStatus {
+        viewModelScope.launch(Dispatchers.Main) {
+            step1()
+            step2()
+        }
+        return object : JobStatus {
+            override fun jobDone(result: Boolean) {
+                viewModelScope.launch {
+                    step3(result)
+                }
+            }
         }
     }
 
-    suspend fun step2(isSuccess: Boolean) = withContext(Dispatchers.IO) {
-        if (isSuccess) {
-            Log.d("rick", "step2")
-        } else {
-            Log.d("rick", "step2 nothing")
-        }
+    private suspend fun step1() = withContext(Dispatchers.IO) {
+        Log.d("rick", "step1")
     }
 
-    suspend fun step3(isSuccess: Boolean) = withContext(Dispatchers.IO) {
+    private suspend fun step2() = withContext(Dispatchers.IO) {
+        Log.d("rick", "step2")
+    }
+
+    private suspend fun step3(isSuccess: Boolean) = withContext(Dispatchers.IO) {
         if (isSuccess) {
             Log.d("rick", "step3")
         } else {
--- app/src/main/java/com/sample/demo/myapplication/MainActivity.kt
+++ app/src/main/java/com/sample/demo/myapplication/MainActivity.kt
@@ -5,13 +5,10 @@ package com.sample.demo.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -22,25 +19,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         send_data.setOnClickListener {
             GlobalScope.launch(Dispatchers.Main) {
-                infoViewModel.step1(true)
-                infoViewModel.step2(true)
-                checkStep3()
+                checkStep3(infoViewModel.exeTask())
             }
         }
     }
-    private fun checkStep3(){
+
+    private fun checkStep3(job: InfoViewModel.JobStatus) {
         AlertDialog.Builder(this@MainActivity)
             .setTitle("hi")
             .setMessage("choice")
             .setPositiveButton("OK") { _, _ ->
-                runBlocking {
-                    infoViewModel.step3(true)
-                }
+                job.jobDone(true)
             }
             .setNegativeButton("NO") { _, _ ->
-                runBlocking {
-                    infoViewModel.step3(false)
-                }
+                job.jobDone(false)
             }
             .show()
     }
